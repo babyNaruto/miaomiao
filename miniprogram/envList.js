4287c546895052823e3c9b22982c64230794805e
Naruto <zhangdz2@foxmail.com>
2021-11-03 19:11:14 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-8g60b6us1907ae02","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-8g60b6us1907ae02 --n quickstartFunctions --r --project "G:\HTML5_Study\HTML_Project\miaomiao" --report_first --report
